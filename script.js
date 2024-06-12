5cee4733050f1de9abb27ffa18778b3fbcce3452
keliphar <gbcliffharsono@gmail.com>
2024-06-12 20:16:30 +0700

    splashscreen fix

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var splashScreen = document.getElementById("splash-screen");
         splashScreen.style.opacity = 0;
-        splashScreen.style.transition = "opacity 0.5s ease";
+        splashScreen.style.visibility = 'hidden';
 
         splashScreen.addEventListener("transitionend", function() {
             splashScreen.style.display = "none";
-            document.getElementById("content").style.display = "block";
+            document.body.classList.add('show-content');
         });
     }, 5000);
 });
